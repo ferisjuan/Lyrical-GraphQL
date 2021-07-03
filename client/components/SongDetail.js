4c5bada36f0ccf422d8703b3e70b3071ddf4d1b3
Juan Feris <feris.juan@gmail.com>
2021-07-03 16:03:53 -0500

    Fetch and navigate to a single song

---
+++ client/components/SongDetail.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+import {Link} from "react-router";
+import {graphql} from "react-apollo";
+
+import fetchSong from "../queries/fetchSong";
+
+class SongDetail extends Component {
+    render() {
+        if (this.props.data.loading) return <div>Loading...</div>;
+
+        const {song} = this.props.data;
+
+        return (
+            <section>
+                <Link to="/">Back</Link>
+                <h3>{song.title}</h3>
+            </section>
+        );
+    }
+}
+
+export default graphql(fetchSong, {
+    options: props => {
+        return {
+            variables: {
+                id: props.params.id
+            }
+        };
+    }
+})(SongDetail);
