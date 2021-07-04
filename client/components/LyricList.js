25e4c72da220d27c2bf2d214efbe9470a078666a
Juan Feris <feris.juan@gmail.com>
2021-07-04 14:17:13 -0500

    Like the lyrics

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,16 +1,34 @@
 import React, {Component} from "react";
 
+import {graphql} from "react-apollo";
+
+import likeLyric from "../queries/likeLyric";
+
 class LyricList extends Component {
+    onLike(id) {
+        this.props.mutate({variables: {id}});
+    }
+
     renderLyrics() {
-        return this.props.lyrics.map(({content, id}) => (
+        return this.props.lyrics.map(({content, id, likes}) => (
             <li key={id} className="collection-item">
                 {content}
+                <div className="vote-box">
+                    <i
+                        className="material-icons"
+                        onClick={() => this.onLike(id)}
+                    >
+                        thumb_up
+                    </i>
+                    {likes}
+                </div>
             </li>
         ));
     }
+
     render() {
         return <ul className="collection">{this.renderLyrics()}</ul>;
     }
 }
 
-export default LyricList;
+export default graphql(likeLyric)(LyricList);
